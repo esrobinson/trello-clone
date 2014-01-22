b199cb6d3844fac2ab593bfdf2e26d95e72ce1ea
Eric Robinson <e.s.robinson@gmail.com>
2014-01-21 22:20:07 -0800

    Autocomplete form with gravatars.  Add seed file

--- app/assets/javascripts/views/boards/add_member.js
+++ app/assets/javascripts/views/boards/add_member.js
@@ -1,21 +1,44 @@
 TrelloClone.Views.AddMember = Backbone.View.extend({
 
+	events: {
+		"submit form#new-member-form": "submit"
+	},
+
 	template: JST["boards/add_member"],
 
 	render: function render(){
+		this.$el.html(this.template());
+		this.setUpTypeahead();
+		return this;
+	},
+
+	setUpTypeahead: function setUpTypeahead(){
 		var members = this.model.get('members');
 		var nonMembers = TrelloClone.users.clone();
+		var engine = {
+		  compile: function(template) {
+		    var compiled = _.template(template);
+
+		    return {
+		      render: function(context) { return compiled(context); }
+		 		}
+		 	}
+		 };
+		var typeaheadTemplate = "<img src=\
+						'<%= Gravtastic(email, {default: 'identicon', size: 20})%>'>\
+						 <%= username %>";
+
 		nonMembers.remove(members.models);
-		console.log(nonMembers)
-		this.$el.html(this.template());
-		this.$("#new-member-input").typeahead([
+    this.$("#new-member-input").typeahead([
 			{
 				name: 'users',
 				local: nonMembers.toJSON(),
-				valueKey: "username"
+				valueKey: "username",
+				limit: 10,
+				engine: engine,
+				template: typeaheadTemplate
 			}
-			]);
-		return this;
+		]);
 	}
 
 });
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+500.times do
+  User.create({ email: Faker::Internet.email, 
+                username: Faker::Internet.user_name,
+                password: Faker::Internet.password
+              })
+end
